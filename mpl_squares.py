00ab67755a1af34c0231f31fde135bcae0f4ca39
dshebeko20 <dshebeko20@gmail.com>
2025-10-25 15:48:32 +0400

    Schedule adjustments.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Задание заголовка диаграмы и меток осей.
-ax.set_title("Square numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of value", fontsize=14)
+ax.set_title("Square numbers", fontsize=40)
+ax.set_xlabel("Value", fontsize=20)
+ax.set_ylabel("Square of value", fontsize=20)
 
 # Задание размера шрифта делений на осях.
-ax.tick_params(labelsize=14)
+ax.tick_params(labelsize=20)
 
 plt.show()
