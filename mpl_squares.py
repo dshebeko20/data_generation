c51f619578623af979175c7aed375d30042d3ebe
dshebeko20 <dshebeko20@gmail.com>
2025-10-25 15:13:42 +0400

    Changing labels and thickness.

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
+
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Задание заголовка диаграмы и меток осей.
+ax.set_title("Square numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+# Задание размера шрифта делений на осях.
+ax.tick_params(labelsize=14)
 
 plt.show()
