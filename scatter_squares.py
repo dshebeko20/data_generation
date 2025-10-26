c983d4b2b341a68f4529c520680e1ade71a35028
dshebeko20 <dshebeko20@gmail.com>
2025-10-26 15:40:39 +0400

    points_1000

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('_mpl-gallery')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Заданиие заголовка диаграммы и меток осей.
 ax.set_title("Square numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Squae of Value", fontsize=14)
 
-# Задание рамера шрита делений на сосях.
-ax.tick_params(labelsize=14)
+# Задание диапазона для каждой оси.
+ax.axis([0, 1100, 0, 1_000_000])
 
 plt.show()
