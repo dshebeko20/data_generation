3e9fb8c599feee315a844ed4b4d16cccfb923b23
dshebeko20 <dshebeko20@gmail.com>
2025-10-26 15:04:16 +0400

    points_5

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
-plt.style.use('_mpl-gallery')
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
+plt.style.use('_mpl-gallery')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 
 # Заданиие заголовка диаграммы и меток осей.
 ax.set_title("Square numbers", fontsize=24)
